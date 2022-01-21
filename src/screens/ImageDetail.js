3d3914e3bd18d204109dd6c0767d30154ecc370f
martin kyalo <martinkkyalo10@gmail.com>
2022-01-22 02:10:57 +0300

    information from parent passed to child component

--- src/screens/ImageDetail.js
+++ src/screens/ImageDetail.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-const ImageDetail = () => {
+// recieve props to pass information from parent to child
+const ImageDetail = (props) => {
+  console.log(props);
   return (
     <View>
-      <Text>Image detail</Text>
+      <Text>{props.title}</Text>
     </View>
   );
 };
