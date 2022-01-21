a2690f9adc049b7bcc96089da190bfa45ee22059
martin kyalo <martinkkyalo10@gmail.com>
2022-01-22 01:26:35 +0300

    props destructured to access the navigation property only

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Hello world</Text>
@@ -10,9 +10,9 @@ const HomeScreen = () => {
         // adding text on a button
         title="Go to Components Demo"
         // detecting user press
-        onPress={() => console.log("Button pressed")}
+        onPress={() => navigation.navigate("Components")}
       />
-      <TouchableOpacity onPress={() => props.navigation.navigate("List")}>
+      <TouchableOpacity onPress={() => navigation.navigate("List")}>
         <Text>Go to List Demo</Text>
       </TouchableOpacity>
     </View>
