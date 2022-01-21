f5f8cca9fe7450da857ae453fda9334c31e9692a
martin kyalo <martinkkyalo10@gmail.com>
2022-01-22 02:21:25 +0300

    showing Image that is saved in local directory

--- src/screens/ImageDetail.js
+++ src/screens/ImageDetail.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 // recieve props to pass information from parent to child
 const ImageDetail = (props) => {
   console.log(props);
   return (
     <View>
+      {/* require statement provide the path of the image location */}
+      <Image source={require("../../assets/beach.jpg")} />
       <Text>{props.title}</Text>
     </View>
   );
