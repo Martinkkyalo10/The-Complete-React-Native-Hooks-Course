704fb556552db5f73d5e11e6fcdac3a8741d447a
martin kyalo <martinkkyalo10@gmail.com>
2022-01-21 22:55:59 +0300

    showing content using react Text, StyleSheet and View elements

---
+++ src/components/ContentDisplay.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+const ContentDisplay = () => {
+  const myName = <Text>Martin Kyalo</Text>;
+  return (
+    <View>
+      <Text style={styles.textStyle}>
+        This a text React Native Primitive Element.
+      </Text>
+      <Text style={styles.subHeaderStyle}>My name is {myName}</Text>
+    </View>
+  );
+};
+
+// component styling
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 35,
+  },
+  subHeaderStyle: {
+    fontSize: 20,
+  },
+});
