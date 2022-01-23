0e94ce3408a8bc521bc55358dbb8cd28d607b9b2
martin kyalo <martinkkyalo10@gmail.com>
2022-01-23 19:32:07 +0300

    tying state values in all the three colors: red, green and bluee

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import ColorCounter from "./../components/ColorCounter";
 
+// adjusting colors
+const COLOR_INCREMENT = 15;
+
 export default function SquareScreen() {
   const [red, setRed] = useState(0);
   const [blue, setBlue] = useState(0);
@@ -10,12 +13,28 @@ export default function SquareScreen() {
     <View>
       {/* add two callback fanctions to increase or decrease color counter */}
       <ColorCounter
-        onIncrease={() => setRed(red + 1)}
-        onDecrease={() => setRed(red - 1)}
+        onIncrease={() => setRed(red + COLOR_INCREMENT)}
+        onDecrease={() => setRed(red - COLOR_INCREMENT)}
         color="Red"
       />
-      <ColorCounter color="Blue" />
-      <ColorCounter color="Greed" />
+      <ColorCounter
+        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
+        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+        color="Blue"
+      />
+      <ColorCounter
+        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
+        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+        color="Green"
+      />
+      <View
+        //   inline style represents an object with hieght, width and backgroundColor as propeties
+        style={{
+          hieght: 150,
+          width: 150,
+          backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+        }}
+      />
     </View>
   );
 }
