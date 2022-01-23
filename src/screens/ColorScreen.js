cd1911c6d164103122f0177bb51ff188ae1e3b68
martin kyalo <martinkkyalo10@gmail.com>
2022-01-23 17:43:04 +0300

    generating a random color and rendering it on color screen

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, View } from "react-native";
 
 export default function ColorScreen() {
+  const [colors, setColors] = useState([]);
   return (
     <View>
-      <Button title="Add a Color" />
+      <Button
+        title="Add a Color"
+        onPress={() => {
+          // the spread operator enable u to expand an object, string
+          //or an array into its indivial elements
+          // the Rest operator does the inverse by reducing a set of elements into one array or object
+          setColors([...colors, randomRgb()]);
+          // the setColors functon creates a brand new array,
+          // the ...colors (spread operator) takes all elements of calor array and adds them inside the new array
+        }}
+      />
       <View style={{ height: 100, width: 100, backgroundColor: randomRgb() }} />
     </View>
   );
