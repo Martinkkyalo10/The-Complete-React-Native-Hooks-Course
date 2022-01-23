1fd26c04e707edede18cd72afdb2ffa90d564c22
martin kyalo <martinkkyalo10@gmail.com>
2022-01-23 21:37:47 +0300

    managing state using useReducer hook applied

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,57 +1,77 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import ColorCounter from "./../components/ColorCounter";
 
 // adjusting colors
 const COLOR_INCREMENT = 15;
 
+//   define reducer
+const reducer = (state, action) => {
+  // state=== {red: number, green: number, blue: number}
+  // action=== {colorToChange: 'red' || 'green' || 'blue', amount: 15||-15 }
+  switch (action.colorToChange) {
+    case "red":
+      return {
+        // making a new object by taking the existing state of the object and copying them into the new one
+        ...state,
+        // overwriting the existing red property
+        red: state.red + action.amount,
+      };
+    case "green":
+      return {
+        // making a new object by taking the existing state of the object and copying them into the new one
+        ...state,
+        // overwriting the existing red property
+        green: state.green + action.amount,
+      };
+
+    case "blue":
+      return {
+        // making a new object by taking the existing state of the object and copying them into the new one
+        ...state,
+        // overwriting the existing red property
+        blue: state.blue + action.amount,
+      };
+
+    default:
+      return state;
+  }
+};
+
 export default function SquareScreen() {
-  const [red, setRed] = useState(0);
-  const [blue, setBlue] = useState(0);
-  const [green, setGreen] = useState(0);
+  // refactoring to manage state using reducer
+  //   state cannot be managed using useState hook abd reducer at the same time
 
-  // helper function to validate state change by ensuring color value is not above 255 and below 0 (-ve value)
-  const setColor = (color, change) => {
-    //   color === 'red', 'blue', 'green'
-    // change== +15, -15
-    switch (color) {
-      case "red":
-        //  using tenary expression
-        red + change > 255 || red + change < 0 ? null : setRed(red + change);
-        return;
-      case "green":
-        //  using tenary expression
-        green + change > 255 || green + change < 0
-          ? null
-          : setGreen(green + change);
-        return;
-      case "blue":
-        //  using tenary expression
-        blue + change > 255 || blue + change < 0
-          ? null
-          : setBlue(blue + change);
-        return;
-      default:
-        return;
-    }
-  };
-
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 }); //this is the initial state
+  const { red, green, blue } = state;
   return (
     <View>
       {/* add two callback fanctions to increase or decrease color counter */}
       <ColorCounter
-        onIncrease={() => setColor("red", COLOR_INCREMENT)}
-        onDecrease={() => setColor("red", -1 * COLOR_INCREMENT)}
+        onIncrease={() =>
+          dispatch({ colorToChange: "red", amount: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: "red", amount: -1 * COLOR_INCREMENT })
+        }
         color="Red"
       />
       <ColorCounter
-        onIncrease={() => setColor("blue", COLOR_INCREMENT)}
-        onDecrease={() => setColor("blue", -1 * COLOR_INCREMENT)}
+        onIncrease={() =>
+          dispatch({ colorToChange: "blue", amount: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: "blue", amount: -1 * COLOR_INCREMENT })
+        }
         color="Blue"
       />
       <ColorCounter
-        onIncrease={() => setColor("green", COLOR_INCREMENT)}
-        onDecrease={() => setColor("green", -1 * COLOR_INCREMENT)}
+        onIncrease={() =>
+          dispatch({ colorToChange: "green", amount: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: "green", amount: -1 * COLOR_INCREMENT })
+        }
         color="Green"
       />
       <View
